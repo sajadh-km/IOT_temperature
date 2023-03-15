d6902c20f3fc29eb654a6477a035ae25eff2fc15
sajadh-km <sajadh.km@infinicomm.io>
2023-03-15 07:51:43 +0530

    json make edited

--- ESP_4/ESP_4.ino
+++ ESP_4/ESP_4.ino
@@ -10,28 +10,13 @@
 #define STASSID "pet_pat"
 #define STAPSK "12345678"
 #endif
-char       payload[3000];
+char       payload[9000];
 
 
 
 typedef struct myData 
 {
-  int fsr1;
-  int fsr2;
-  int fsr3;
-  int fsr4;
-  int fsr5;
-  int fsr6;
-  int fsr7;
-  int fsr8;
-  int fsr9;
-  int fsr10;
-  int fsr11;
-  int fsr12;
-  int fsr13;
-  int fsr14;
-  int fsr15;
-  int fsr16;
+  int fsr[101];
   int pulse;
   float temperature_1;
   float temperature_2;
@@ -41,11 +26,10 @@ DATAS raw_data;
 int faulty_structure_detected=0;
 void make_JSON()
 {
-  if(raw_data.fsr16 > 179 || raw_data.fsr16 <28)
+  int itr;
+  if(raw_data.fsr[100] > 179 || raw_data.fsr[100] <28)
   {
-      memset(&raw_data, 0x00, sizeof(myData));
-      //Serial.print("structure faulty");
-      //delay(100);//ESP.restart();
+      memset(&raw_data, 0x00, sizeof(raw_data));
       faulty_structure_detected=1;
       return;
   }
@@ -53,171 +37,36 @@ void make_JSON()
   {
     faulty_structure_detected=0;
   }
-  
- char  num[10];
-  memset(payload, 0x00, 370);
-  strcat(payload, "[{\"animalName\": \"Not available\",\"age\":0,");
-  
-  strcat(payload, "\"pressureSensors\":[");
-  
-  strcat(payload, "{");
-  strcat(payload, "\"sensorLocationX\": 3,\"sensorLocationY\": 5,");
-  strcat(payload, "\"sensorStatus\": true,");
-  strcat(payload, "\"pressureValue\": ");
-  sprintf(num, "%d",raw_data.fsr1);  //C1
-  strcat(payload,  num);
-  strcat(payload, "}");
-  strcat(payload, ","  );
-  
-  strcat(payload, "{");
-  strcat(payload, "\"sensorLocationX\": 4,\"sensorLocationY\":5,");
-  strcat(payload, "\"sensorStatus\": true,");
-  strcat(payload, "\"pressureValue\": ");
-  sprintf(num, "%d",raw_data.fsr2);  //C2
-  strcat(payload,  num);
-  strcat(payload, "}");
-  strcat(payload, ","  );
-  
-  strcat(payload, "{");
-  strcat(payload, "\"sensorLocationX\": 6,\"sensorLocationY\":5,");
-  strcat(payload, "\"sensorStatus\": true,");
-  strcat(payload, "\"pressureValue\": ");
-  sprintf(num, "%d",raw_data.fsr3);  //C3
-  strcat(payload,  num);
-  strcat(payload, "}");
-  strcat(payload, ","  );
-  
-  
-  strcat(payload, "{");
-  strcat(payload, "\"sensorLocationX\": 7,\"sensorLocationY\":5,");
-  strcat(payload, "\"sensorStatus\": true,");
-  strcat(payload, "\"pressureValue\": ");
-  sprintf(num, "%d",raw_data.fsr4); //C4
-  strcat(payload,  num);
-  strcat(payload, "}");
-  strcat(payload, ","  );
-  
-  
-  strcat(payload, "{");
-  strcat(payload, "\"sensorLocationX\": 3,\"sensorLocationY\":6,");
-  strcat(payload, "\"sensorStatus\": true,");
-  strcat(payload, "\"pressureValue\": ");
-  sprintf(num, "%d",raw_data.fsr5);  //D1
-  strcat(payload,  num);
-  strcat(payload, "}");
-  strcat(payload, ","  );
-  
-  strcat(payload, "{");
-  strcat(payload, "\"sensorLocationX\": 4,\"sensorLocationY\":6,");
-  strcat(payload, "\"sensorStatus\": true,");
-  strcat(payload, "\"pressureValue\": ");
-  sprintf(num, "%d",raw_data.fsr6);  //D2
-  strcat(payload,  num);
-  strcat(payload, "}");
-  strcat(payload, ","  );
-  
-  strcat(payload, "{");
-  strcat(payload, "\"sensorLocationX\": 6,\"sensorLocationY\":6,");
-  strcat(payload, "\"sensorStatus\": true,");
-  strcat(payload, "\"pressureValue\": ");
-  sprintf(num, "%d",raw_data.fsr7);  //D3
-  strcat(payload,  num);
-  strcat(payload, "}");
-  strcat(payload, ","  );
-  
-  strcat(payload, "{");
-  strcat(payload, "\"sensorLocationX\": 7,\"sensorLocationY\":6,");
-  strcat(payload, "\"sensorStatus\": true,");
-  strcat(payload, "\"pressureValue\": ");
-  sprintf(num, "%d",raw_data.fsr8);  //D4
-  strcat(payload,  num);
-  strcat(payload, "}");
-  strcat(payload, ","  );
-  
-  strcat(payload, "{");
-  strcat(payload, "\"sensorLocationX\": 3,\"sensorLocationY\":8,");
-  strcat(payload, "\"sensorStatus\": true,");
-  strcat(payload, "\"pressureValue\": ");
-  sprintf(num, "%d",raw_data.fsr9);  //B1
-  strcat(payload,  num);
-  strcat(payload, "}");
-  strcat(payload, ","  );
-  
-  strcat(payload, "{");
-  strcat(payload, "\"sensorLocationX\": 4,\"sensorLocationY\":8,");
-  strcat(payload, "\"sensorStatus\": true,");
-  strcat(payload, "\"pressureValue\": ");
-  sprintf(num, "%d",raw_data.fsr10);  //B2
-  strcat(payload,  num);
-  strcat(payload, "}");
-  strcat(payload, ","  );
-  
-  
-  strcat(payload, "{");
-  strcat(payload, "\"sensorLocationX\": 6,\"sensorLocationY\":8,");
-  strcat(payload, "\"sensorStatus\": true,");
-  strcat(payload, "\"pressureValue\": ");
-  sprintf(num, "%d",raw_data.fsr11);  //B3
-  strcat(payload,  num);
-  strcat(payload, "}");
-  strcat(payload, ","  );
-  
-  
-  strcat(payload, "{");
-  strcat(payload, "\"sensorLocationX\": 7,\"sensorLocationY\":8,");
-  strcat(payload, "\"sensorStatus\": true,");
-  strcat(payload, "\"pressureValue\": ");
-  sprintf(num, "%d",raw_data.fsr12);   //B4
-  strcat(payload,  num);
-  strcat(payload, "}");
-  strcat(payload, ","  );
-  
-  
-  strcat(payload, "{");
-  strcat(payload, "\"sensorLocationX\": 3,\"sensorLocationY\":9,");
-  strcat(payload, "\"sensorStatus\": true,");
-  strcat(payload, "\"pressureValue\": ");
-  sprintf(num, "%d",raw_data.fsr13);  //A1
-  strcat(payload,  num);
-  strcat(payload, "}");
-  strcat(payload, ","  );
-  
-  
-  
-  strcat(payload, "{");
-  strcat(payload, "\"sensorLocationX\": 4,\"sensorLocationY\":9,");
-  strcat(payload, "\"sensorStatus\": true,");
-  strcat(payload, "\"pressureValue\": ");
-  sprintf(num, "%d",raw_data.fsr14);  //A2
-  strcat(payload,  num);
-  strcat(payload, "}");
-  strcat(payload, ","  );
-  
-  
-  
-  strcat(payload, "{");
-  strcat(payload, "\"sensorLocationX\": 6,\"sensorLocationY\":9,");
-  strcat(payload, "\"sensorStatus\": true,");
-  strcat(payload, "\"pressureValue\": ");
-  sprintf(num, "%d",raw_data.fsr15);  //A3
-  strcat(payload,  num);
-  strcat(payload, "}");
-  strcat(payload, ","  );
-  
-  
-  
-  strcat(payload, "{");
-  strcat(payload, "\"sensorLocationX\": 7,\"sensorLocationY\":9,");
-  strcat(payload, "\"sensorStatus\": true,");
-  strcat(payload, "\"pressureValue\": ");
-  sprintf(num, "%d",raw_data.fsr16);  //A4
-  strcat(payload,  num);
-  strcat(payload, "}");  
-  
-  
-  strcat(payload, "]," );
 
-  strcat(payload, "\"temperatureSensors\":[{");
+  char  num[10];
+  short int loc_x;
+  short int loc_y;
+  memset(payload, 0x00, sizeof(payload));
+  strcat(payload, "[{\"animalName\": \"Not available\",\"age\":0,");
+  strcat(payload, "\"pressureSensors\":[");
+  for(itr = 1; itr < 101; itr++)
+  {
+      loc_y = (itr-1) /10;
+      loc_x = (itr-1) %10;
+
+      strcat(payload, "{");
+      strcat(payload, "\"sensorLocationX\": ");
+      sprintf(num, "%d",loc_x);  //3
+      strcat(payload,  num);
+      strcat(payload,",\"sensorLocationY\": ");
+      sprintf(num, "%d",loc_y);  //5
+      strcat(payload,  num);
+      strcat(payload,",");
+      strcat(payload, "\"sensorStatus\": true,");
+      strcat(payload, "\"pressureValue\": ");
+      sprintf(num, "%d",raw_data.fsr[itr]);  //C1
+      strcat(payload,  num);
+      strcat(payload, "}");
+      if(itr < 100)
+      strcat(payload, ","  );
+  }
+  strcat(payload, "]," );
+    strcat(payload, "\"temperatureSensors\":[{");
   strcat(payload, "\"sensorLocationX\": 5,\"sensorLocationY\":6,");
   strcat(payload, "\"temperature\":");
   sprintf(num, "%0.1f", raw_data.temperature_1);
@@ -231,9 +80,9 @@ void make_JSON()
   sprintf(num, "%d", 100);
   strcat(payload, num);
   strcat(payload, "}]");
-  
+
   strcat(payload,  ","  );
-  
+
   strcat(payload, "\"pulseSensors\":[{");
   strcat(payload, "\"sensorLocationX\": 5,\"sensorLocationY\":5,");
   strcat(payload, "\"pulse\":");
@@ -246,10 +95,9 @@ void make_JSON()
   strcat(payload, "\"maxPulse\":");
   strcat(payload, "94");
   strcat(payload, "}]");
-  
-
   strcat(payload, "}]");
- }
+
+}
 
 
 int parse_data()
